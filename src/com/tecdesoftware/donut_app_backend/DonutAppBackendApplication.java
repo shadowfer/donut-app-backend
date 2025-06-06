b8bcd52639e05930c8348052e569fafdebb3f02d
JJJEEEz <juanestebanperezvelazquez@gmail.com>
2025-06-06 13:14:54 -0600

    First commit

---
+++ src/com/tecdesoftware/donut_app_backend/DonutAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.donut_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonutAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonutAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/com/tecdesoftware/donut_app_backend/DonutAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.donut_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonutAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
